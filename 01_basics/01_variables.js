75001f6f71ae292684802cfebee1dbdbcd139090
kunal9430 <91870213+kunal9430@users.noreply.github.com>
2023-10-13 18:45:04 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 1222
+let accountEmail = "kunalkkumar2Egmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,6 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3); 
+
+console.log(typeof(undefined))
+console.log(typeof null);
