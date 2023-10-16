91413c46b371ec3ed731e35f026608157d08494b
kunal9430 <91870213+kunal9430@users.noreply.github.com>
2023-10-16 10:20:51 +0000

    More way to convert in array

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,17 @@
+let arr1 = [1,3,4];
+let arr2 = [5,6,7];
+
+// arr1.push(arr2);
+// console.log(arr1);
+
+// arr1 = arr1.concat(arr2,arr1,arr2,arr1);
+// console.log(arr1);
+
+// arr1 = [...arr1, ...arr2, ...[1,3,4,5]];
+// console.log(arr1);
+
+// let arr = [1,2,3,4,5, [1,66,44,33,3],[11,11,44,666,[4,6,7]]];
+// arr  = arr.flat(1);
+// arr  = arr.flat(Infinity);
+// console.log(arr);
+// console.log(Array.isArray(arr));
