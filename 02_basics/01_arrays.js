ab5d652f5952b8ae70937b63fc7f90f3618b376b
kunal9430 <91870213+kunal9430@users.noreply.github.com>
2023-10-16 07:38:27 +0000

    Arrays in javascript

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,38 @@
+// array
+
+const myArr = [0,1,2,3,4,5];
+myArr.push(6);
+myArr.push(7);
+
+// console.log(myArr);
+
+myArr.pop();
+// console.log(myArr);
+
+myArr.unshift(9);
+// console.log(myArr);
+
+myArr.shift();
+// console.log(myArr);
+
+// console.log(myArr.includes(4));
+// console.log(myArr.indexOf(3));
+
+// let newArray = myArr.join();
+// console.log(newArray);
+// console.log(typeof newArray); // array to string
+// console.log(newArray.split(','));  //string to array
+
+//slice vs splice
+
+// let arr = [0,3,3,4,2,3];
+// //with slice
+// let arr1 = arr.slice(2,5); // this nither modifies the original array nor include the last index
+// console.log(arr);
+// console.log(arr1);
+// //with splice
+// let arr2 = arr.splice(2,5); //this modifies the original array and also include the last index
+// console.log(arr);
+// console.log(arr2);
+
+
