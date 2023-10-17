ae503e1ee2ee439554c07e879ecdeabb55d12ed7
kunal9430 <91870213+kunal9430@users.noreply.github.com>
2023-10-17 09:39:41 +0000

    fucntion

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,48 @@
+// functions
+//function functionName(){}
+
+//adding of two number
+function addTwoNumber(number1,number2){ //parameter
+    console.log("Add : ",number1+number2);
+}
+// addTwoNumber(3,4);
+// addTwoNumber(3,"a");
+// addTwoNumber(3,null);//arguments
+
+// function loginUserMessage(username){
+//     if(!username){
+//         console.log("Enter the name");
+//         return;
+//     }
+//     return `${username} just logged in`;
+// }
+
+// console.log(loginUserMessage("kunal"));
+// console.log(loginUserMessage());
+
+
+//passing default values in function
+// function loginUserMessage(username = "Megha Singh"){
+//     if(!username){
+//         console.log("Enter the name");
+//         return;
+//     }
+//     return `${username} just logged in`;
+// }
+// console.log(loginUserMessage("kunal"));
+// console.log(loginUserMessage());
+
+
+// without rest operator
+// function calculate(num){
+//     return num;
+// }
+// console.log(calculate(12))
+// console.log(calculate(-1000,14, 15, 13,88));
+//with rest operator
+// function calculate(...num){
+//     return num;
+// }
+// console.log(calculate(12))
+// console.log(calculate(-1000,14, 15, 13,88));
+
