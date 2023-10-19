31956085c66c620780eb24a5246f28bd10551c1d
kunal9430 <91870213+kunal9430@users.noreply.github.com>
2023-10-19 09:41:11 +0000

    setting all changes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,75 @@
+//closure in js
+//closure is the property by which the inner fuction can acces the scope of outer fucntion
+
+function one(){
+    const a = 200;
+    const b = "Kunal"
+
+    function two(){
+        const c = b;
+        let d = a + 10;
+        console.table([c,d]);
+    }
+    two();
+
+}
+one();
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
